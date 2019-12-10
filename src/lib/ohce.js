4368557259775f7112a4f9af188d0311f84e6a16
Javier Manzano <javier.manzano22@gmail.com>
2019-12-10 20:23:49 +0100

    refactor(): ohce must be a class to avoid multiple instances could lead into race conditions

--- src/lib/ohce.js
+++ src/lib/ohce.js
@@ -4,28 +4,36 @@ const MORNING = 'morning';
 const AFTERNOON = 'afternoon';
 const NIGHT = 'night';
 
-const getDaytime = ({ time }) => {
-  const hour = time.getHours();
-  if (hour >= 6 && hour < 12) {
-    return MORNING;
-  }
-  if (hour >= 12 && hour < 20) {
-    return AFTERNOON;
-  }
-  return NIGHT;
-};
-
 const greetings = {
   morning: 'Buenos días',
   afternoon: 'Buenas tardes',
   night: 'Buenos días',
 };
 
+class Ohce {
+  constructor() {
+    this.name = '';
+  }
+
+  getDaytime({ time }) {
+    const hour = time.getHours();
+    if (hour >= 6 && hour < 12) {
+      return MORNING;
+    }
+    if (hour >= 12 && hour < 20) {
+      return AFTERNOON;
+    }
+    return NIGHT;
+  };
+
+  init({ time, input }) {
+    this.name = input;
+    return [`${greetings[this.getDaytime({ time })]} ${this.name}`];
+  }
+}
+
 
 module.exports = {
   greetings,
-  init: ({ time, input }) => {
-    name = input;
-    return [`${greetings[getDaytime({ time })]} ${name}`];
-  },
+  Ohce,
 };
--- test/ohce.test.js
+++ test/ohce.test.js
@@ -1,37 +1,59 @@
 /* global describe, it */
 require('chai').should();
 const { expect } = require('chai');
-const { init, greetings } = require('../src/lib/ohce');
+const { Ohce, greetings } = require('../src/lib/ohce');
 
 
 describe('ohce tests', () => {
+	var ohce;
+
+	beforeEach(() => {
+		ohce = new Ohce()
+	});
+
+
   describe('Get correct greeting depending on the current time', () => {
     it('The response must be an array', () => {
       const time = new Date('2019-12-10T18:30:00.000Z');
       const input = 'Tomás';
-      const result = init({ time, input });
+      const result = ohce.init({ time, input });
       expect(Array.isArray(result)).to.equal(true);
     });
 
     it(`Must say ${greetings.morning}`, () => {
       const time = new Date('2019-12-10T10:30:00.000Z');
       const input = 'Tomás';
-      const result = init({ time, input });
+      const result = ohce.init({ time, input });
       expect(result[0]).to.equal(`${greetings.morning} ${input}`);
     });
 
     it(`Must say ${greetings.afternoon}`, () => {
       const time = new Date('2019-12-10T18:30:00.000Z');
       const input = 'Tomás';
-      const result = init({ time, input });
+      const result = ohce.init({ time, input });
       expect(result[0]).to.equal(`${greetings.afternoon} ${input}`);
     });
 
     it(`Must say ${greetings.night}`, () => {
       const time = new Date('2019-12-10T21:30:00.000Z');
       const input = 'Tomás';
-      const result = init({ time, input });
+      const result = ohce.init({ time, input });
       expect(result[0]).to.equal(`${greetings.night} ${input}`);
     });
-  });
+	});
+	
+
+
+	describe('Concurrency does not affect to Ohce instances', () => {
+		it(`Must say ${greetings.morning} Tomás & ${greetings.morning} Torralvo`, () => {
+      const time = new Date('2019-12-10T10:30:00.000Z');
+      const input1 = 'Tomás';
+      let result = ohce.init({ time, input: input1 });
+			expect(result[0]).to.equal(`${greetings.morning} ${input1}`);
+			const secondOhce = new Ohce();
+			const input2 = 'Torralvo';
+      result = secondOhce.init({ time, input: input2 });
+      expect(result[0]).to.equal(`${greetings.morning} ${input2}`);
+    });
+	});
 });
